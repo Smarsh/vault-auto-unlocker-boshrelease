315e4f73bb87ad7b603b485cec26360436e53fa1
John Dee <john.dee@smarsh.com>
2019-12-17 11:11:01 -0700

    adding 10 second sleep before running unseal task to ensure the server comes up

--- jobs/vault-unlock/templates/post-start.sh
+++ jobs/vault-unlock/templates/post-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sleep 10
 <% p('vault.unseal_keys').each do |key| -%>
 /var/vcap/packages/vault/bin/vault operator unseal -address=https://localhost:<%= p('vault.port') %> -tls-skip-verify <%= key %>
 <% end %>
